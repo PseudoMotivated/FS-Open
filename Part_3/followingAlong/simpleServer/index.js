c52aceb6224ca465440f780f39841be2da46f26b
pseudomotivated <eabudy8@protonmail.com>
2025-04-09 12:32:13 +0200

    Made CORS work

--- Part_3/followingAlong/simpleServer/index.js
+++ Part_3/followingAlong/simpleServer/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
 
-// app.use( (request, response, next) => {
-//   response.header("Access-Control-Allow-Origin", "https://firecamp.dev"); // update to match the domain you will make the request from
-//   // response.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
-//   // response.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "https://firecamp.dev"); // update to match the domain you will make the request from
+  response.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
+  response.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json())
-
-
 let notes = [
   {
     id: "1",
@@ -31,17 +30,18 @@ let notes = [
 
 generateId = () => {
   const maxId = notes.length > 0
-  ? Math.max(
-    ...notes.map(
-      n => Number(n.id)  // Takes the notes array and makes it into an array of id's.
-    )                    // Then it finds the max. If max is undefined; returns zero.
-  )                      // Assigns maxId whatever is returned.
-  : 0;
+    ? Math.max(
+      ...notes.map(
+        n => Number(n.id)  // Takes the notes array and makes it into an array of id's.
+      )                    // Then it finds the max. If max is undefined; returns zero.
+    )                      // Assigns maxId whatever is returned.
+    : 0;
   return String(maxId + 1);
 }
+
 app.post('/api/notes', (request, response) => {
   const body = request.body
-console.log(body)
+  console.log(body)
   if (!body.content) {
     return response.status(400).json({
       error: "Content Missin"
@@ -53,6 +53,7 @@ console.log(body)
     important: body.important || false,
     id: generateId()
   }
+
   console.log(note)
   notes = notes.concat(note)   // Adds note with a one higher id.
   response.json(note)
@@ -81,6 +82,7 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end()
   }
 })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`)
