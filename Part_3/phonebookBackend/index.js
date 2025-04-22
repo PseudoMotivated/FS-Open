313458a02ae0660d5e85b9b5b9a8e2b853707acd
PseudoMotivated <122965701+PseudoMotivated@users.noreply.github.com>
2025-04-22 12:17:05 +0200

    Completed up to 3.5

--- Part_3/phonebookBackend/index.js
+++ Part_3/phonebookBackend/index.js
@@ -3,6 +3,7 @@ const app = express()
 
 app.use((request, response, next) => {
   response.header("Access-Control-Allow-Origin", "https://firecamp.dev"); // update to match the domain you will make the request from
+  response.header("Access-Control-Allow-Origin", "http://firecamp.dev"); // update to match the domain you will make the request from
   response.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
   response.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -49,7 +50,46 @@ app.get('/api/persons' , (request, response) => {
     response.json(persons)
 })
 
+app.post('/api/persons' , (request, response) => {
+  console.log(request.body)
+  person = request.body
+  console.log(person)
+  person = [
+    {
+      id: Math.round(Math.random() * 10000 ).toString(),
+      name: person.name? person.name : "" ,
+      number: person.number? person.number : "0",
+    }
+  ]
+  persons = persons.concat(person)
+  console.log(persons)
+  response.json(person)
+  response.status(200).end()
+})
 
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  let person = persons.find(person => person.id === id) 
+  if (person) {
+    response.json(person)
+  }else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  let person = persons.find(person => person.id === id) 
+  console.log(persons)
+  if (person) {
+    persons = persons.filter(person => person.id != id)
+    console.log(persons)
+    response.status(200).end()
+  }else {
+    response.status(404).end()
+  }
+})
 
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people.</p>
