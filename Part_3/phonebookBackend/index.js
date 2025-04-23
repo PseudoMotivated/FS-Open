2653f20fdbc00ad640989fa1beff9ac918265394
PseudoMotivated <122965701+PseudoMotivated@users.noreply.github.com>
2025-04-23 13:50:50 +0200

    Finished exercises up to 3.8

--- Part_3/phonebookBackend/index.js
+++ Part_3/phonebookBackend/index.js
@@ -1,15 +1,36 @@
 const express = require('express')
-const app = express()
 
-app.use((request, response, next) => {
-  response.header("Access-Control-Allow-Origin", "https://firecamp.dev"); // update to match the domain you will make the request from
-  response.header("Access-Control-Allow-Origin", "http://firecamp.dev"); // update to match the domain you will make the request from
-  response.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
-  response.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+var morgan = require('morgan')
+
+morgan.token("postdata", (request, response) => {
+  return request.data;
+})
+
+// app.use((request, response, next) => {
+//   response.header("Access-Control-Allow-Origin", "https://firecamp.dev"); // update to match the domain you will make the request from
+//   response.header("Access-Control-Allow-Origin", "http://firecamp.dev"); // update to match the domain you will make the request from
+//   response.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
+//   response.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+const app = express()
 app.use(express.json())
+// app.use(requestLogger)
+app.use(morgan(':postdata :method :url :status length :res[content-length] - :response-time ms'))
+// app.use(unknownEndpoint)
 
 
 let persons = [
@@ -35,38 +56,45 @@ let persons = [
     }
 ]
 
-generateId = () => {
-  const maxId = persons.length > 0
-  ? Math.max(
-    ...notes.map(
-      n => Number(n.id)  // Takes the notes array and makes it into an array of id's.
-    )                    // Then it finds the max. If max is undefined; returns zero.
-  )                      // Assigns maxId whatever is returned.
-  : 0;
-  return String(maxId + 1);
-}
-
 app.get('/api/persons' , (request, response) => {
     response.json(persons)
 })
 
 app.post('/api/persons' , (request, response) => {
-  console.log(request.body)
-  person = request.body
+  let person = request.body
   console.log(person)
+  // Check has name
+  if (!person.name || person.name === ""){
+    response.status(400)
+    response.json({error:"Must have a name."})
+    return
+  }
+  if (!person.number){
+    response.status(400)
+    response.json({error:"Must have a number."})
+    return
+  }
+
+  // Check name unique
+  if (
+    persons.some(p => p.name === person.name)
+  ){
+    response.status(400)
+    response.json({error:"Name must be unique."})
+    return
+  }
+
   person = [
     {
-      id: Math.round(Math.random() * 10000 ).toString(),
-      name: person.name? person.name : "" ,
+      id: Math.round(Math.random() * 100000 ).toString(),
+      name: person.name,
       number: person.number? person.number : "0",
     }
   ]
   persons = persons.concat(person)
-  console.log(persons)
   response.json(person)
   response.status(200).end()
 })
-
 
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
@@ -97,12 +125,9 @@ app.get('/info', (request, response) => {
   `)
 })
 
-
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
-
 
 const PORT = 3001
 app.listen(PORT, () => {
